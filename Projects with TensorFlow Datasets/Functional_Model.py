202392c4a73354933b6d7073ab5a457a96b05854
Cabbar Gorkem Gul <gorkemgtech@gmail.com>
2022-12-12 21:50:50 +0300

    update sequential & functional models

--- Projects with TensorFlow Datasets/Functional_Model.py
+++ Projects with TensorFlow Datasets/Functional_Model.py
@@ -1,27 +1,40 @@
-#%% Libraries
+# Import Libraries
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.models import Sequential
+from keras import Input, Model
 from tensorflow.keras.datasets import mnist
-#%% Loading mnist data and reshaping
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train.reshape(-1, 28*28).astype('float32') / 255.0
-x_test = x_test.reshape(-1, 28*28).astype('float32') / 255.0
-#%%
-#Creating Functional Model
-inputs = tf.keras.Input(shape = (784))
-x = Dense(512, activation = 'relu')(inputs)
-x = Dense(256, activation = 'relu')(x)
-outputs = Dense(10, activation = 'softmax')(x)
-model = tf.keras.Model(inputs = inputs, outputs = outputs)
-#%% Model Compiling
+
+# In case using a GPU 
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+# Load the mnist data and reshape it
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train = X_train.reshape(-1, 28 * 28).astype('float32') / 255.0
+X_test = X_test.reshape(-1, 28 * 28).astype('float32') / 255.0
+
+# Create a Functional Model
+def func_model():
+    inputs = Input(shape = (784))
+    x = Dense(512, activation = 'relu')(inputs)
+    x = Dense(256, activation = 'relu')(x)
+    outputs = Dense(10, activation = 'softmax')(x)
+    model = Model(inputs = inputs, outputs = outputs)
+    return model
+
+model = func_model()
+
+# Compile the model
 model.compile(loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = False),
               optimizer = tf.keras.optimizers.Adam(lr=0.001),
               metrics = ['accuracy'])
-#%% model fit
-batch_size = 32
-epoch = 5
-model.fit(x_train, y_train, batch_size = batch_size, epochs = epoch, verbose = 2)
-model.evaluate(x_test, y_test, batch_size = batch_size, verbose = 2)
-#%% model summary
-print(model.summary())
+
+# Define the params and fit the model 
+BATCH_SIZE = 32
+epochs = 5
+
+model.fit(X_train, y_train, batch_size = BATCH_SIZE, epochs = epochs, verbose = 2)
+model.evaluate(X_test, y_test, batch_size = BATCH_SIZE, verbose = 2)
+
+# Print out the model summary
+print(f"Model Summary:\n {model.summary()}")
--- Projects with TensorFlow Datasets/Sequential_Model.py
+++ Projects with TensorFlow Datasets/Sequential_Model.py
@@ -1,27 +1,36 @@
-#%% Libraries
+# Import Libraries
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.datasets import mnist
-#%% Loading mnist data and reshaping
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train.reshape(-1, 28*28).astype('float32') / 255.0
-x_test = x_test.reshape(-1, 28*28).astype('float32') / 255.0
-#%%
-#Creating Sequential Model
+
+# In case using GPU 
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+# Load the mnist data and reshape it
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train = X_train.reshape(-1, 28 * 28).astype('float32') / 255.0
+X_test = X_test.reshape(-1, 28 * 28).astype('float32') / 255.0
+
+# Create a Sequential Model
 model = Sequential([
     Dense(512, activation = tf.nn.relu),
-    Dense(256, activation = tf.nn.relu),
+    Dense(256, activation = 'relu'),
     Dense(10)
 ])
-#%% Model Compiling
+
+# Compile the model
 model.compile(loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True),
               optimizer = tf.keras.optimizers.Adam(),
               metrics = ['accuracy'])
-#%% model fit
-batch_size = 32
-epoch = 5
-model.fit(x_train, y_train, batch_size = batch_size, epochs = epoch, verbose = 2)
-model.evaluate(x_test, y_test, batch_size = batch_size, verbose = 2)
-#%% model summary
-print(model.summary())
+
+# Set up the params and fit the model
+BATCH_SIZE = 32
+epochs = 5
+
+model.fit(X_train, y_train, batch_size = BATCH_SIZE, epochs = epochs, verbose = 2)
+model.evaluate(X_test, y_test, batch_size = BATCH_SIZE, verbose = 2)
+
+# Check out the model summary
+print(f"Model Summary:\n {model.summary()}")
